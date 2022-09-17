9d0590d78ab1f257579828d5de6bf8bf668b0d8b
Matías Huenul <matias.huenul.07@gmail.com>
2022-09-16 21:36:39 -0300

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,98 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import React, { useState } from "react";
+import * as React from "react";
+import SignIn from "./components/LogIn";
+import {
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import { fakeAuthProvider } from "./common/auth";
 
-const API_URL = process.env.REACT_APP_API_URL;
+let AuthContext = React.createContext();
 
-function App() {
-  let [text, setText] = useState("");
-  let makeRequest = (e) => {
-    e.preventDefault();
-    fetch(`${API_URL}/hola`)
-      .then(res => res.text())
-      .then(message => setText(message));
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          onClick={makeRequest}
-          style={{cursor:'pointer'}}
-          >
-          Make request
-        </a>
-        <p>
-          {text}
-        </p>
-      </header>
-    </div>
-  );
+function useAuth() {
+  return React.useContext(AuthContext);
 }
 
-export default App;
+function AuthProvider({ children }) {
+  let [user, setUser] = React.useState(null);
+
+  let signin = (newUser, callback) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
+
+  let signout = (callback) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
+
+  let value = { user, signin, signout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+function RequireAuth({ children }) {
+  let auth = useAuth();
+  let location = useLocation();
+
+  if (!auth.user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+function LoginPage() {
+  let navigate = useNavigate();
+  let location = useLocation();
+  let auth = useAuth();
+
+  let from = location.state?.from?.pathname || "/";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let formData = new FormData(event.currentTarget);
+    let username = formData.get("email");
+
+    auth.signin(username, () => {
+      navigate(from, { replace: true });
+    });
+  }
+
+  return <SignIn handleSubmit={handleSubmit}></SignIn>
+}
+
+function PublicPage() {
+  return <h1>Public</h1>;
+}
+
+function ProtectedPage() {
+  return <h1>Protected</h1>;
+}
+
+export default function App() {
+    return (
+      <AuthProvider>
+        <Routes>
+              <Route path="/" element={<PublicPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route
+              path="/protected"
+              element={
+                  <RequireAuth>
+                  <ProtectedPage />
+                  </RequireAuth>
+              }
+              />
+        </Routes>
+      </AuthProvider>
+    );
+}
