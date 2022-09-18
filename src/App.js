c72d19a61172ed6fb93e91b8d79d468e8d4fc574
Matías Huenul <43656633+matias-huenul@users.noreply.github.com>
2022-09-18 14:57:06 -0300

    Login screen (#3)
    * Add login page
    * Add Auth context and Dashboard

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/Auth";
+import LogIn from "./components/LogIn";
+import Dashboard from "./components/Dashboard";
 
-const API_URL = process.env.REACT_APP_API_URL;
-
-function App() {
-  let [text, setText] = useState("");
-  let makeRequest = (e) => {
-    e.preventDefault();
-    fetch(`${API_URL}/hola`)
-      .then(res => res.text())
-      .then(message => setText(message));
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          onClick={makeRequest}
-          style={{cursor:'pointer'}}
-          >
-          Make request
-        </a>
-        <p>
-          {text}
-        </p>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LogIn />} />
+          <Route path="/protected" element={<Dashboard />} />
+        </Routes>
+      </AuthProvider>
+    );
 }
-
-export default App;
