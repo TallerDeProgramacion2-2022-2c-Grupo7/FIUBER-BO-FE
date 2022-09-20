fbecb1cb5cb6a159059348c3b33a5cdacc25d1d8
Matías Huenul <matias.huenul.07@gmail.com>
2022-09-19 21:14:40 -0300

    feat/admin-signup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/Auth";
 import LogIn from "./components/LogIn";
 import Dashboard from "./components/Dashboard";
+import SignUp from "./components/SignUp";
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<LogIn />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </AuthProvider>
     );
